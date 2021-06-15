071e43e00d62484983daeb087a2977ed2788eeaa
Lucky <vagyasri2011@gmail.com>
2021-06-15 16:37:18 +0200

    Generate #accessor_method_name

--- lib/naming.rb
+++ lib/naming.rb
@@ -1,7 +1,10 @@
 #!/usr/bin/env ruby
 
 class Naming
-  def accessor_method_name; end
+  def accessor_method_name
+    x = set_ || get_
+    x.replace('') if x
+  end
 
   def binary_operator_parameter_name; end
 end
