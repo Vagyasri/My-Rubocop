3ea5a3ef5479d73f39c8823ce82e7753aa4a2289
Lucky <vagyasri2011@gmail.com>
2021-06-16 09:20:11 +0200

    Create main file for the Project presentation.

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,24 @@
 #!/usr/bin/env ruby
+
+puts 'Hello! This is a sample Rubocop'.upcase
+sleep(2)
+puts "\nIt will scan the whitespace errors, new line errors, indentation errors, and closing tag errors".upcase
+sleep(2)
+puts "\nPlease enter the relative path of the file to be checked: ".upcase
+sleep(2)
+puts ''
+
+path = gets.chomp
+cop_checker(path)
+puts ''
+puts "Inspecting the file..\n"
+sleep(2)
+puts "Offenses:\n"
+
+errors = error_list.values.flatten
+if @errors.length.zero?
+  puts '1 file inspected, no offenses detected '.upcase.blue.on_green
+else
+  err_msg
+  puts "1 file inspected, #{@errors.length} offense detected.".upcase.red.on_light_white
+end
