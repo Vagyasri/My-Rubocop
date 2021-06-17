458d53b5129dd2e8673696594b4eeb5541d08cac
Lucky <vagyasri2011@gmail.com>
2021-06-17 19:05:55 +0200

    Add colors to the errors in err_msgs

--- lib/error_msg.rb
+++ lib/error_msg.rb
@@ -1,39 +1,39 @@
 require 'colorize'
 
 def layout_ami
-  puts "#{path}:#{line}:#{char}: C: [Correctable] Layout/AccessModifierIndentation: Indent access modifiers like private."
+  puts "#{path}".cyan + ":#{line}:#{char}:" + "C".yellow + ":" + "[Correctable]".yellow + "Layout/AccessModifierIndentation: Indent access modifiers like" + "private".yellow + "."
 end
 
 def layout_arg
-    puts "#{path}:#{line}:#{char}: C: [Correctable] Layout/ArgumentAlignment: Align the arguments of a method call if they span more than one line."
+    puts "#{path}".cyan + ":#{line}:#{char}:" + "C".yellow + ":" + "[Correctable]".yellow + "Layout/ArgumentAlignment: Align the arguments of a method call if they span more than one line."
 end 
 
 def layout_arr
-    puts "#{path}:#{line}:#{char}: C: [Correctable] Layout/ArrayAlignment: Align the elements of an array literal if they span more than one line."
+    puts "#{path}".cyan + ":#{line}:#{char}:" + "C".yellow + ":" + "[Correctable]".yellow + "Layout/ArrayAlignment: Align the elements of an array literal if they span more than one line."
 end 
 
 # def layout_length
-#     puts "#{path}:#{line}:#{char}: C: [Correctable] Layout/ 
+#     puts "#{path}".cyan + ":#{line}:#{char}:" + "C".yellow + ":" + "[Correctable]".yellow + "Layout/ 
     
 #     "
 # end 
 
 def lint_amb
-    puts "#{path}:#{line}:#{char}: C: [Correctable] Lint/AmbiguousAssignment: Suspicious assignment detected. Did you mean -=?"
+    puts "#{path}".cyan + ":#{line}:#{char}:" + "C".yellow + ":" + "[Correctable]".yellow + "Lint/AmbiguousAssignment: Suspicious assignment detected. Did you mean" + "-=".yellow "?"
 end 
 
 def naming_method
-    puts "#{path}:#{line}:#{char}: C: Naming/AccessorMethodName: Do not prefix reader method names with get_."
+    puts "#{path}".cyan + ":#{line}:#{char}:" + "C".yellow + ": Naming/AccessorMethodName: Do not prefix reader method names with" + "get_".yellow + "."
 end
 
 def naming_operator
-    puts "#{path}:#{line}:#{char}: C: [Correctable] Naming/BinaryOperatorParameterName: When defining the + operator, name its argument other."
+    puts "#{path}".cyan + ":#{line}:#{char}:" + "C".yellow + ":" + "[Correctable]".yellow + "Naming/BinaryOperatorParameterName: When defining the" + "+".yellow + "operator, name its argument" + "other".yellow + "."
 end  
 
 def style_mod
-    puts "#{path}:#{line}:#{char}: C: Style/AccessModifierDeclarations: private should not be inlined in method definitions."
+    puts "#{path}".cyan + ":#{line}:#{char}:" + "C".yellow + ": Style/AccessModifierDeclarations:" + "private".yellow + "should not be inlined in method definitions."
 end
 
 def style_grp
-    puts "#{path}:#{line}:#{char}: C: [Correctable] Style/AccessorGrouping: Group together all attr_reader attributes."
+    puts "#{path}".cyan + ":#{line}:#{char}:" + "C".yellow + ":" + "[Correctable]".yellow + "Style/AccessorGrouping: Group together all" + "attr_reader".yellow + "attributes."
 end
